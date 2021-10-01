53c47872aaaf645fcac26195203bdbf6936c1c87
Zijian Xu <znxuz@proton.me>
2021-10-01 09:16:31 +0200

    [java] 111. Minimum Depth BST: recursive return min(depth(left), depth(right))

---
+++ Java/minDepthBST/Solution.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+package minDepthBST;
+
+import utils.TreeNode;
+
+class Solution {
+
+	// TODO iterative DFS
+
+    public int minDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null)
+			return 1;
+		if (root.left == null)
+			return minDepth(root.right) + 1;
+		if (root.right == null)
+			return minDepth(root.left) + 1;
+		return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
