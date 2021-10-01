67f3102ad58ddaf4b77162e72e84c1ae111409fc
Zijian Xu <znxuz@proton.me>
2021-10-01 08:47:03 +0200

    [java] 7. Reverse Integer: check overflow using MAX/MIN values

---
+++ Java/reverseInteger/Solution.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/reverse-integer/
+
+package reverseInteger;
+
+class Solution {
+
+    public int reverse(int x) {
+		int rev = 0;
+
+		while (x != 0) {
+			int last = x % 10;
+			if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && last > 7))
+				return 0;
+			if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && last < -8))
+				return 0;
+			rev = rev * 10 + last;
+			x /= 10;
+		}
+
+		return rev;
+    }
+}
