019331dab33bb96bb706cc6ae5bfe2b7f398e254
Zijian Xu <znxuz@proton.me>
2021-11-18 13:16:13 +0100

    [java] 657. Robot Return To Origin: x & y index return if x == y == 0

---
+++ Java/robotReturnToOrigin/Solution.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/robot-return-to-origin/
+
+package robotReturnToOrigin;
+
+class Solution {
+
+	public boolean judgeCircle(String moves) {
+		int x, y;
+		x = y = 0;
+		for (char c : moves.toCharArray()) {
+			switch (c) {
+				case 'U':
+					y++;
+					break;
+				case 'D':
+					y--;
+					break;
+				case 'L':
+					x--;
+					break;
+				case 'R':
+					x++;
+					break;
+			}
+		}
+
+		return x == 0 && y == 0;
+	}
+}
