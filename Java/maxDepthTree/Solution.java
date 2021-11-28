530bf26dea7c1463766dde60cd8b720793fba380
Zijian Xu <znxuz@proton.me>
2021-11-28 15:42:09 +0100

    [Revisit] [java] 104. Max Depth Tree: iterative BFS via a queue and max counter

--- Java/maxDepthTree/Solution.java
+++ Java/maxDepthTree/Solution.java
@@ -2,20 +2,46 @@
 
 package maxDepthTree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 import utils.TreeNode;
 
 class Solution {
 
+    public int maxDepth(TreeNode root) {
+		return maxDepthIter(root);
+	}
+
+	// time complexity: O(n)
+	// space complexity: O(n)
 	public int maxDepthIter(TreeNode root) {
-		// TODO itertive DFS
-		return 0;
+		if (root == null)
+			return 0;
+
+		Deque<TreeNode> queue = new ArrayDeque<>();
+		queue.offerLast(root);
+		int max = 0;
+		while (!queue.isEmpty()) {
+			int count = queue.size();
+			for (int i = 0; i < count; i++) {
+				var node = queue.pollFirst();
+				if (node.left != null)
+					queue.offerLast(node.left);
+				if (node.right != null)
+					queue.offerLast(node.right);
+			}
+			max++;
+		}
+
+		return max;
 	}
 
 	// time complextity: O(n)
 	// space complextity: O(n)
-    public int maxDepth(TreeNode root) {
+    public int maxDepthRec(TreeNode root) {
 		if (root == null)
 			return 0;
-		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+		return 1 + Math.max(maxDepthRec(root.left), maxDepthRec(root.right));
     }
 }
