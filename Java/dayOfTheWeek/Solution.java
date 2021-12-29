24afe0ac63db4a635d3dcb052ec288985f3bd85f
Zijian Xu <znxuz@proton.me>
2021-12-29 16:51:14 +0100

    [java] 1185. Day Of The Week: used built-in API

---
+++ Java/dayOfTheWeek/Solution.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/day-of-the-week/
+
+package dayOfTheWeek;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+
+    public String dayOfTheWeek(int day, int month, int year) {
+		LocalDate date = LocalDate.of(year, month, day);
+		return date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+    }
+}
